3bb959c9c4def7dab19fec7585d6c49e4638ae03
Franck <gwadafrank@gmail.com>
2022-11-05 01:22:33 +0300

    request data from api

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,23 @@
+require "json"
+require "net/http"
+require "open-uri"
 # Write your code here
+class GetRequester
+    attr_accessor :url
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(URL)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+    
+    def parse_json
+        res = JSON.parse(get_response_body)
+        res.collect do |location|
+            location
+        end
+    end
+end
